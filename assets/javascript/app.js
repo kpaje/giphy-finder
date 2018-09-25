a1aa7ee279d9ea1d7cfca31075e57773c3b99fb6
kpcruz <kelvinmembership@gmail.com>
2018-09-25 14:57:07 -0700

    moved queryURL to assemble object, created view object, moved array into handlers for memory efficiency, reworded a bit for better semantics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,4 @@
-peopleArray = [
-    'Rihanna',
-    'Dr. Dre',
-    '50 Cent',
-    'P!nk',
-];
-
 var assemble = {
-    buttons: function (button, index, array) {
-        button.addClass('gifButton btn btn-danger mb-2');
-        button.attr('data-person', array[index]);
-        button.text(array[index]);
-    },
-    gifs: function (results, index, gifDiv) {
-        //set attributes before prepending
-        var animated = results[index].images.fixed_height.url;
-        var still = results[index].images.fixed_height_still.url;
-        var personImage = $('<img>');
-        personImage.attr('src', results[index].images.fixed_height.url);
-        personImage.attr({
-            'src'          : still,
-            'data-still'   : still,
-            'data-animate' : animated,
-            'data-state'   : 'still',
-            'class'        : 'gif',
-        });
-        gifDiv.append(personImage);
-        $('#gifs').prepend(gifDiv);
-    },
-    rating: function (results, index, gifDiv) {
-        var rating = results[index].rating;
-        var text = $('<p>').text('Rating: ' + rating);
-        gifDiv.append(text);
-    },
-};
-
-var actions = {
     queryURL: function (query) {
         //partitioned url for future changes
         let person = $(query).attr('data-person');
@@ -44,19 +8,40 @@ var actions = {
         let result = giphySearch + person + apiKey + imageCount;
         return result;
     },
-    addButtons: function (array) {
-        $('.gifButton').remove();
-        for (var i = 0; i < array.length; i++) {
-            var button = $('<button>');
-            assemble.buttons(button, i, array);
-            $('#newButton').append(button);
-        };
+    buttons: function (button, index, array) {
+        button.addClass('gifButton btn btn-danger mb-2');
+        button.attr('data-person', array[index]);
+        button.text(array[index]);
     },
+    gifs: function (results, index, gifDiv) {
+        //set attributes before prepending
+        var animatedGif = results[index].images.fixed_height.url;
+        var stillGif = results[index].images.fixed_height_still.url;
+        var personImage = $('<img>');
+        personImage.attr('src', results[index].images.fixed_height.url);
+        personImage.attr({
+            'src'          : stillGif,
+            'data-still'   : stillGif,
+            'data-animate' : animatedGif,
+            'data-state'   : 'still',
+            'class'        : 'gif',
+        });
+        gifDiv.append(personImage);
+        $('#gifs').prepend(gifDiv);
+    },
+    ratings: function (results, index, gifDiv) {
+        var rating = results[index].rating;
+        var text = $('<p>').text('Rating: ' + rating);
+        gifDiv.append(text);
+    },
+};
+
+var actions = {
     addGifs: function (results) {
         for (var i = 0; i < results.length; i++) {
             if (results[i].rating !== 'r') {
                 var gifDiv = $('<div>');
-                assemble.rating(results, i, gifDiv)
+                assemble.ratings(results, i, gifDiv)
                 assemble.gifs(results, i, gifDiv)
             };
         };
@@ -79,18 +64,22 @@ var actions = {
         // And don't you dare say it isn't true
         // As long as that song's gettin' airplay I'm dissin' you
         // I'm a hair away from getting carried away and getting sued
-        // I was gonna stop at sixteen, that was thirty-two
-        // This is thirty-four bars, we ain't even third of the way through
         var mariahTroll = $('<button class="gifButton btn btn-danger mb-2" data-person="jk">Mariah Carey</button>')
         $('#newButton').append(mariahTroll);
     },
 };
 
 var handlers = {
+    buttonArray: [
+        'Rihanna',
+        'Dr. Dre',
+        '50 Cent',
+        'P!nk',
+    ],
     generateGifs: $(document).on('click', '.gifButton', function () {
         actions.setAsActive(this);
         $.ajax({
-            url: actions.queryURL(this),
+            url: assemble.queryURL(this),
             method: 'GET'
         })
         .then(function (response) {
@@ -99,8 +88,8 @@ var handlers = {
         });
     }),
     toggleGif: $(document).on('click', '.gif', function () {
-        var state = $(this).attr("data-state");
-        if (state === 'still') {
+        var gifState = $(this).attr("data-state");
+        if (gifState === 'still') {
             actions.animateGif(this);
         } else {
             actions.pauseGif(this);
@@ -108,14 +97,25 @@ var handlers = {
     }),
     submitButton: $(document).on('click', '#submit', function (event) {
         event.preventDefault();
-        var input = $('#input').val();
-        if (input.length > 2) {
-            peopleArray.push(input);
-            actions.addButtons(peopleArray);
+        var userInput = $('#input').val();
+        if (userInput.length > 2) {
+            handlers.buttonArray.push(userInput);
+            view.displayButtons(handlers.buttonArray);
             $('#input').val('');
         };
     }),
 };
 
-actions.addButtons(peopleArray);
+var view = {
+    displayButtons: function (array) {
+        $('.gifButton').remove();
+        for (var i = 0; i < array.length; i++) {
+            var button = $('<button>');
+            assemble.buttons(button, i, array);
+            $('#newButton').append(button);
+        };
+    },
+};
+
+view.displayButtons(handlers.buttonArray);
 actions.mariahTroll();
